0b5e148371f7cb7ffed3d44c50db833222ed3332
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2018-12-17 13:20:07 +0000

    Set settings for tabFormControl

--- DashboardViewer/Form1.Designer.cs
+++ DashboardViewer/Form1.Designer.cs
@@ -29,31 +29,28 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabFormControl1 = new DevExpress.XtraBars.TabFormControl();
+            this.tabFormControl_Main = new DevExpress.XtraBars.TabFormControl();
             this.tabFormDefaultManager1 = new DevExpress.XtraBars.TabFormDefaultManager();
             this.barDockControlTop = new DevExpress.XtraBars.BarDockControl();
             this.barDockControlBottom = new DevExpress.XtraBars.BarDockControl();
             this.barDockControlLeft = new DevExpress.XtraBars.BarDockControl();
             this.barDockControlRight = new DevExpress.XtraBars.BarDockControl();
-            this.tabFormContentContainer1 = new DevExpress.XtraBars.TabFormContentContainer();
-            this.tabFormPage1 = new DevExpress.XtraBars.TabFormPage();
-            ((System.ComponentModel.ISupportInitialize)(this.tabFormControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabFormControl_Main)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.tabFormDefaultManager1)).BeginInit();
             this.SuspendLayout();
             // 
-            // tabFormControl1
+            // tabFormControl_Main
             // 
-            this.tabFormControl1.Location = new System.Drawing.Point(0, 0);
-            this.tabFormControl1.Manager = this.tabFormDefaultManager1;
-            this.tabFormControl1.Name = "tabFormControl1";
-            this.tabFormControl1.Pages.Add(this.tabFormPage1);
-            this.tabFormControl1.SelectedContainer = this.tabFormContentContainer1;
-            this.tabFormControl1.SelectedPage = this.tabFormPage1;
-            this.tabFormControl1.Size = new System.Drawing.Size(284, 50);
-            this.tabFormControl1.TabForm = this;
-            this.tabFormControl1.TabIndex = 0;
-            this.tabFormControl1.TabStop = false;
-            this.tabFormControl1.OuterFormCreating += new DevExpress.XtraBars.OuterFormCreatingEventHandler(this.OnOuterFormCreating);
+            this.tabFormControl_Main.AllowMoveTabsToOuterForm = false;
+            this.tabFormControl_Main.Location = new System.Drawing.Point(0, 0);
+            this.tabFormControl_Main.Manager = this.tabFormDefaultManager1;
+            this.tabFormControl_Main.Name = "tabFormControl_Main";
+            this.tabFormControl_Main.ShowTabsInTitleBar = DevExpress.XtraBars.ShowTabsInTitleBar.True;
+            this.tabFormControl_Main.Size = new System.Drawing.Size(827, 27);
+            this.tabFormControl_Main.TabForm = this;
+            this.tabFormControl_Main.TabIndex = 0;
+            this.tabFormControl_Main.TabStop = false;
+            this.tabFormControl_Main.OuterFormCreating += new DevExpress.XtraBars.OuterFormCreatingEventHandler(this.OnOuterFormCreating);
             // 
             // tabFormDefaultManager1
             // 
@@ -61,66 +58,55 @@
             this.tabFormDefaultManager1.DockControls.Add(this.barDockControlBottom);
             this.tabFormDefaultManager1.DockControls.Add(this.barDockControlLeft);
             this.tabFormDefaultManager1.DockControls.Add(this.barDockControlRight);
+            this.tabFormDefaultManager1.DockingEnabled = false;
             this.tabFormDefaultManager1.Form = this;
-            this.tabFormDefaultManager1.MaxItemId = 0;
             // 
             // barDockControlTop
             // 
             this.barDockControlTop.CausesValidation = false;
             this.barDockControlTop.Dock = System.Windows.Forms.DockStyle.Top;
-            this.barDockControlTop.Location = new System.Drawing.Point(0, 0);
-            this.barDockControlTop.Size = new System.Drawing.Size(284, 0);
+            this.barDockControlTop.Location = new System.Drawing.Point(0, 27);
+            this.barDockControlTop.Manager = null;
+            this.barDockControlTop.Size = new System.Drawing.Size(827, 0);
             // 
             // barDockControlBottom
             // 
             this.barDockControlBottom.CausesValidation = false;
             this.barDockControlBottom.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.barDockControlBottom.Location = new System.Drawing.Point(0, 261);
-            this.barDockControlBottom.Size = new System.Drawing.Size(284, 0);
+            this.barDockControlBottom.Location = new System.Drawing.Point(0, 465);
+            this.barDockControlBottom.Manager = null;
+            this.barDockControlBottom.Size = new System.Drawing.Size(827, 0);
             // 
             // barDockControlLeft
             // 
             this.barDockControlLeft.CausesValidation = false;
             this.barDockControlLeft.Dock = System.Windows.Forms.DockStyle.Left;
-            this.barDockControlLeft.Location = new System.Drawing.Point(0, 0);
-            this.barDockControlLeft.Size = new System.Drawing.Size(0, 261);
+            this.barDockControlLeft.Location = new System.Drawing.Point(0, 27);
+            this.barDockControlLeft.Manager = null;
+            this.barDockControlLeft.Size = new System.Drawing.Size(0, 438);
             // 
             // barDockControlRight
             // 
             this.barDockControlRight.CausesValidation = false;
             this.barDockControlRight.Dock = System.Windows.Forms.DockStyle.Right;
-            this.barDockControlRight.Location = new System.Drawing.Point(284, 0);
-            this.barDockControlRight.Size = new System.Drawing.Size(0, 261);
-            // 
-            // tabFormContentContainer1
-            // 
-            this.tabFormContentContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabFormContentContainer1.Location = new System.Drawing.Point(0, 50);
-            this.tabFormContentContainer1.Name = "tabFormContentContainer1";
-            this.tabFormContentContainer1.Size = new System.Drawing.Size(284, 211);
-            this.tabFormContentContainer1.TabIndex = 1;
-            // 
-            // tabFormPage1
-            // 
-            this.tabFormPage1.ContentContainer = this.tabFormContentContainer1;
-            this.tabFormPage1.Name = "tabFormPage1";
-            this.tabFormPage1.Text = "Page 0";
+            this.barDockControlRight.Location = new System.Drawing.Point(827, 27);
+            this.barDockControlRight.Manager = null;
+            this.barDockControlRight.Size = new System.Drawing.Size(0, 438);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(827, 465);
             this.Controls.Add(this.barDockControlLeft);
             this.Controls.Add(this.barDockControlRight);
             this.Controls.Add(this.barDockControlBottom);
             this.Controls.Add(this.barDockControlTop);
-            this.Controls.Add(this.tabFormContentContainer1);
-            this.Controls.Add(this.tabFormControl1);
+            this.Controls.Add(this.tabFormControl_Main);
             this.Name = "Form1";
-            this.TabFormControl = this.tabFormControl1;
+            this.TabFormControl = this.tabFormControl_Main;
             this.Text = "Form1";
-            ((System.ComponentModel.ISupportInitialize)(this.tabFormControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tabFormControl_Main)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.tabFormDefaultManager1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -129,14 +115,12 @@
 
         #endregion
 
-        private DevExpress.XtraBars.TabFormControl tabFormControl1;
+        private DevExpress.XtraBars.TabFormControl tabFormControl_Main;
         private DevExpress.XtraBars.TabFormDefaultManager tabFormDefaultManager1;
         private DevExpress.XtraBars.BarDockControl barDockControlTop;
         private DevExpress.XtraBars.BarDockControl barDockControlBottom;
         private DevExpress.XtraBars.BarDockControl barDockControlLeft;
         private DevExpress.XtraBars.BarDockControl barDockControlRight;
-        private DevExpress.XtraBars.TabFormContentContainer tabFormContentContainer1;
-        private DevExpress.XtraBars.TabFormPage tabFormPage1;
 
     }
 }
