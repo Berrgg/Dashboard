c51d29695c11382793318a300bbf58d9a8964c2c
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2018-12-27 15:46:37 +0000

    Add new class NewTabForm and event handler when one of buttons is clicked

---
+++ DashboardViewer/View/NewTabForm.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.XtraEditors;
+using DevExpress.XtraLayout;
+using System;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashboardViewer.View
+{
+    public class NewTabForm : XtraUserControl
+    {
+        public string FilePath { get; private set; }
+
+        public NewTabForm()
+        {
+            TextEdit textEditName = new TextEdit();
+            TextEdit textEditPath = new TextEdit();
+
+            textEditPath.Click += new EventHandler(textEditPath_Click);
+
+            var lc = new LayoutControl
+            {
+                Dock = DockStyle.Fill
+            };
+
+
+
+        }
+
+        private void textEditPath_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new XtraOpenFileDialog())
+            {
+                dialog.Title = "New dashboard page.";
+                dialog.Filter = "XML files(*.xml)|*.xml";
+
+                if (dialog.ShowDialog() != DialogResult.OK)
+                    return;
+
+                try
+                {
+                    FilePath = dialog.InitialDirectory + dialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error when selected XML file. Error message: " + ex.Message);
+                }
+            }
+        }
+    }
+}
