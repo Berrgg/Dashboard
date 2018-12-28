17e496fc8d944067ccb4e63cffe99380f5c9c4e9
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2018-12-28 12:10:09 +0000

    Add a new class CustomPageNameValidationRule to validate user input text

---
+++ DashboardViewer/Helper/CustomPageNameValidationRule.cs
@@ -0,0 +1,23 @@
+﻿using DevExpress.XtraEditors.DXErrorProvider;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace DashboardViewer.Helper
+{
+    public class CustomPageNameValidationRule : ValidationRule
+    {
+        public CustomPageNameValidationRule()
+        {
+            CaseSensitive = false;
+            ErrorText = "Page name cannot be blank or contains '-'";
+            ErrorType = ErrorType.Critical;
+        }
+        public override bool Validate(Control control, object value)
+        {
+            var pageName = value.ToString();
+            bool result = pageName != string.Empty && !pageName.ToLower().Contains('-');
+
+            return result;
+        }
+    }
+}
