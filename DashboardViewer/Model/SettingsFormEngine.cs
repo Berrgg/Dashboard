ff12db2473e0f19360fb638f950e8410d97128f6
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-01-20 16:53:21 +0000

    Add new class SettingsFormEngine responsible for executing settings form

---
+++ DashboardViewer/Model/SettingsFormEngine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashboardViewer.Model
+{
+    public class SettingsFormEngine
+    {
+        public void Run(ISettingsForm settingsForm)
+        {
+            settingsForm.Execute();
+        }
+    }
+}
