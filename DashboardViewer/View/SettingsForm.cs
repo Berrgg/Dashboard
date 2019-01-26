4fab3ba4c28e7455641063b1a8bbaaa41d4bf6b7
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-01-26 11:07:43 +0000

    Imolement intherface into SettingsForm class

--- DashboardViewer/View/SettingsForm.cs
+++ DashboardViewer/View/SettingsForm.cs
@@ -1,14 +1,34 @@
-﻿using DevExpress.XtraEditors;
+﻿using DashboardViewer.Model;
+using DevExpress.XtraEditors;
 using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DashboardViewer.View
 {
-    public class SettingsForm : XtraUserControl
+    public class SettingsForm : XtraUserControl, ISettingsForm
     {
+        public void Execute()
+        {
+            throw new NotImplementedException();
+        }
 
+        public bool IsFormValid()
+        {
+            throw new NotImplementedException();
+        }
+
+        public NameValueCollection KeyValueCollection()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ValidForm()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
