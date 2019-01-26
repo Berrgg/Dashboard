b4b4ebdbdaa0e9b643db5d7023986ee32325de15
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-01-26 13:32:50 +0000

    Add properties and controls to SettingsForm

--- DashboardViewer/View/SettingsForm.cs
+++ DashboardViewer/View/SettingsForm.cs
@@ -1,6 +1,8 @@
 ﻿using DashboardViewer.Model;
 using DevExpress.XtraEditors;
+using DevExpress.XtraLayout;
 using System;
+using System.Windows.Forms;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
@@ -11,6 +13,23 @@ namespace DashboardViewer.View
 {
     public class SettingsForm : XtraUserControl, ISettingsForm
     {
+        private TextEdit textEditRefresh = new TextEdit() { Name = "textEditRefresh" };
+        private ToggleSwitch toggleSwitchAutoRefresh = new ToggleSwitch() { Name = "toggleSwitchAutoRefresh"};
+        private ToggleSwitch toggleSwitchAutoRotate = new ToggleSwitch() { Name = "toggleSwitchAutoRefresh" };
+
+        public SettingsForm()
+        {
+
+            var lc = new LayoutControl();
+            lc.Dock = DockStyle.Fill;
+            lc.AddItem("Dashboard refresh time (in seconds):", textEditRefresh).TextVisible=true;
+            lc.AddItem("Dashboard auto refresh:", toggleSwitchAutoRefresh).TextVisible = true;
+            lc.AddItem("Auto rotate between dashboards:", toggleSwitchAutoRotate).TextVisible = true;
+
+            Controls.Add(lc);
+            Dock = DockStyle.None;
+            Width = 500;
+        }
         public void Execute()
         {
             throw new NotImplementedException();
