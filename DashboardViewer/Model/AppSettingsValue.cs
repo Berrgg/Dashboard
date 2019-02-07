fb2922fe15a892859081baa9fdb04e2ed0baec45
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-02-07 14:46:30 +0000

    Adding a new class AppSettingsValue which implements ISettingsValue

---
+++ DashboardViewer/Model/AppSettingsValue.cs
@@ -0,0 +1,18 @@
+﻿using MyApp.Model;
+
+namespace DashboardViewer.Model
+{
+    public class AppSettingsValue : ISettingsValue
+    {
+        private string _settingsValue;
+
+        public AppSettingsValue(string value)
+        {
+            _settingsValue=value
+        }
+        public string GetSettingsValue()
+        {
+            return _settingsValue;
+        }
+    }
+}
