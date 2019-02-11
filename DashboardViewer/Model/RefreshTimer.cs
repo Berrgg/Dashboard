87991422ac283565f8eb28c1977bff6ef1a61169
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-02-11 12:47:36 +0000

    Adding class RefreshTimer

---
+++ DashboardViewer/Model/RefreshTimer.cs
@@ -0,0 +1,41 @@
+﻿using MyApp.Model;
+using System.Timers;
+using System.Threading.Tasks;
+
+namespace DashboardViewer.Model
+{
+    public class RefreshTimer : IDashboardTimer
+    {
+        private DevExpress.DashboardWin.DashboardViewer _viewer;
+        public Timer DashboardTimer { get; private set; }
+
+        public RefreshTimer(DevExpress.DashboardWin.DashboardViewer viewer)
+        {
+            _viewer = viewer;
+            Timer DashboardTimer = new Timer();
+            DashboardTimer.Elapsed += async (sender, e) => await DashboardTimerElapsedAsync();
+            DashboardTimer.Enabled = true;
+        }
+
+        public void Execute()
+        {
+            var settings = new TabFormSettings("GeneralAppSettings");
+            var isEnabled = bool.Parse(settings.GetValue("AutoRefresh"));
+
+            if (isEnabled)
+            {
+                var reloadTime = int.Parse(settings.GetValue("RefreshTime"))*1000;
+                DashboardTimer.Interval = reloadTime;
+                DashboardTimer.Start();
+            }
+        }
+
+        private async Task DashboardTimerElapsedAsync()
+        {
+            await Task.Run(() =>
+            {
+                _viewer.ReloadData(true);
+            });
+        }
+    }
+}
