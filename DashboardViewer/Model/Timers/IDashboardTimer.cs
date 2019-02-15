7b5dcc43e711efb3b9ac38c2cfa6da3c371672f3
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-02-15 14:28:54 +0000

    Add back classes

---
+++ DashboardViewer/Model/Timers/IDashboardTimer.cs
@@ -0,0 +1,11 @@
+﻿using System.Timers;
+
+namespace DashboardViewer.Model
+{
+    public interface IDashboardTimer
+    {
+        void Execute();
+
+        void DashboardTimerElapsed(object sender, ElapsedEventArgs e);
+    }
+}
---
+++ DashboardViewer/Model/Timers/RefreshTimer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Timers;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+using MyApp.Model;
+
+namespace DashboardViewer.Model
+{
+    public class RefreshTimer : BaseTimer, IDashboardTimer
+    {
+        private DevExpress.DashboardWin.DashboardViewer _viewer;
+
+        public RefreshTimer(TabFormControl tabForm)
+        {
+            TabFormControl = tabForm;
+
+            SetTimer();
+        }
+
+        public void Execute()
+        {
+            var settings = new TabFormSettings("GeneralAppSettings");
+            var isEnabled = bool.Parse(settings.GetValue("AutoRefresh"));
+
+            if (isEnabled)
+            {
+                var reloadTime = int.Parse(settings.GetValue("RefreshTime"))*1000;
+                DashboardTimer.Interval = reloadTime;
+                DashboardTimer.Start();
+            }
+        }
+
+        public override void DashboardTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            TabFormControl.BeginInvoke(new Action(() =>
+            {
+                foreach (Control c in TabFormControl.SelectedPage.ContentContainer.Controls)
+                {
+                    if (c is DevExpress.DashboardWin.DashboardViewer)
+                        _viewer = (c as DevExpress.DashboardWin.DashboardViewer);
+                }
+
+                _viewer.ReloadData(true);
+            }));
+        }
+    }
+}
