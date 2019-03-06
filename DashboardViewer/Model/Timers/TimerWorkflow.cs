18506719daa261d39dcd8bf28a4de978942d242c
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-03-06 16:42:58 +0000

    Add TimerWorkflow class to create list of timers on main file

---
+++ DashboardViewer/Model/Timers/TimerWorkflow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace DashboardViewer.Model.Timers
+{
+    public class TimerWorkflow : ITimerWorkflow
+    {
+        private readonly List<IDashboardTimer> _timersList;
+
+        public TimerWorkflow()
+        {
+            _timersList = new List<IDashboardTimer>();
+        }
+
+        public void Add(IDashboardTimer dashboardTimer)
+        {
+            _timersList.Add(dashboardTimer);
+        }
+
+        public void Remove(IDashboardTimer dashboardTimer)
+        {
+            _timersList.Remove(dashboardTimer);
+        }
+
+        public IEnumerable<IDashboardTimer> GetTimers()
+        {
+            return _timersList;
+        }
+    }
+}
