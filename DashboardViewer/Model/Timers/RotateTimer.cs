19a8654d6416af8fd5a9d3b902b559b9a333e89b
Krzysztof Matyja <matyja.krzysztof@gmail.com>
2019-03-07 11:53:12 +0000

    Check is timer enabled in RotateTimer method DashboardTimerElapsed

--- DashboardViewer/Model/Timers/RotateTimer.cs
+++ DashboardViewer/Model/Timers/RotateTimer.cs
@@ -24,26 +24,29 @@ namespace DashboardViewer.Model.Timers
 
         public override void DashboardTimerElapsed(object sender, ElapsedEventArgs e)
         {
-            var index = 0;
-            var pageIndex = TabFormControl.Pages.IndexOf(TabFormControl.SelectedPage);
-            var maxIndex = TabFormControl.Pages.Count - 1;
-
-            if (maxIndex > 0)
+            if (IsTimerEnabled)
             {
-                if (pageIndex == maxIndex)
-                    index = 0;
+                var index = 0;
+                var pageIndex = TabFormControl.Pages.IndexOf(TabFormControl.SelectedPage);
+                var maxIndex = TabFormControl.Pages.Count - 1;
+
+                if (maxIndex > 0)
+                {
+                    if (pageIndex == maxIndex)
+                        index = 0;
+                    else
+                        index += 1;
+                }
                 else
-                    index += 1;
-            }
-            else
-            {
-                index = 0;
-            }
+                {
+                    index = 0;
+                }
 
-            TabFormControl.BeginInvoke(new Action(() =>
-            {
-                TabFormControl.SelectedPage = TabFormControl.Pages[index];
-            }));
+                TabFormControl.BeginInvoke(new Action(() =>
+                {
+                    TabFormControl.SelectedPage = TabFormControl.Pages[index];
+                }));
+            }
         }
     }
 }
